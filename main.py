d8e27ca5c0f77798db4d13e18ce5e2f06f43501e
mithra795 <mithra795@gmail.com>
2025-08-03 14:29:27 -0500

    added logic to open and close game window

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    number = 1
+    while number > 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        number += 1
+        screen.fill("black")
+        pygame.display.flip()
+        number -= 1
+        
 
 
 if __name__ == "__main__":
