8306cb11ff917b8c4aabbd1cc91bbec5c47d6511
mithra795 <mithra795@gmail.com>
2025-08-04 11:27:15 -0500

    added in ship shooting and collision detection removal of shots and asteroids

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init
@@ -22,15 +23,17 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     ship = Player(x, y)
     field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                returnd 
             
 
         updatable.update(dt)
@@ -38,6 +41,11 @@ def main():
             if thing.collisions(ship) == True:
                 print("Game over!")
                 sys.exit()
+        for thing in asteroids:
+            for shot in shots:
+                if thing.collisions(shot) == True:
+                    thing.kill()
+                    shot.kill()
         screen.fill("black")
         for thing in drawable:
             thing.draw(screen)
