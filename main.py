83b3f981fa4a84ca6cfd4c7dbf07333d854dee36
mithra795 <mithra795@gmail.com>
2025-08-03 19:15:32 -0500

    completed the player constructor, i can now draw a triangle on screen

--- main.py
+++ main.py
@@ -3,21 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
+    fps = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     number = 1
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    ship = Player(x, y)
     while number > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         number += 1
         screen.fill("black")
+        ship.draw(screen)
         pygame.display.flip()
+        dt = (fps.tick(60) / 1000)
         number -= 1
         
 
